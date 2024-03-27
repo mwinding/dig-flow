72b3fe4ea0c6419c542cf4144cd314698ac95e16
Michael Winding <michael.winding@crick.ac.uk>
2024-03-27 12:00:21 +0000

    modified input parameters

--- scripts/plugcamera_pipeline.py
+++ scripts/plugcamera_pipeline.py
@@ -3,9 +3,9 @@ import argparse
 
 # pulling user-input variables from command line
 parser = argparse.ArgumentParser(description='plugcamera pipeline: transferring data from RPis to NEMO, initial processing')
-parser.add_argument('-e', '--experiment-name', dest='experiment_name', action='store', type=str, required=True, help='location of video file')
-parser.add_argument('-l', '--rig-list', nargs='+', type=int, default=None, help='list of rig names')
-parser.add_argument('-ip', '--ip-path', dest='ip_path', action='store', type=str, required=True)
+parser.add_argument('-e', '--experiment-name', dest='experiment_name', action='store', type=str, required=True, help='name of experiment')
+parser.add_argument('-l', '--rig-list', nargs='+', type=int, default=None, help='list of rig names if only a specific subset will be used')
+parser.add_argument('-ip', '--ip-path', dest='ip_path', action='store', type=str, default=None, help='path to ip_address list')
 parser.add_argument('-p', '--pipeline', dest='pipeline', action='store', type=int, required=True)
 
 # ingesting user-input arguments
@@ -19,4 +19,3 @@ exp = dig.Experiment(experiment_name=experiment_name, exp_type='plugcamera', rig
 
 if(pipeline==1): exp.pc_pipeline1()
 if(pipeline==2): exp.pc_pipeline2()
-if(pipeline==3): exp.pc_pipeline3()
