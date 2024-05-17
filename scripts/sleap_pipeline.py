8db05a7e70d8eaadbd5847d072632aa7b9dc5f5e
Michael Winding <michael.winding@crick.ac.uk>
2024-05-17 23:28:42 +0100

    added missing parameter

--- scripts/sleap_pipeline.py
+++ scripts/sleap_pipeline.py
@@ -7,8 +7,8 @@ parser.add_argument('-p', '--predictions-path', dest='predictions_path', action=
 parser.add_argument('-v', '--video-path', dest='video_path', action='store', type=str, default=None, help='path to folder with video(s)')
 parser.add_argument('-m1', '--centroid-path', dest='centroid_path', action='store', type=str, default=None, help='path to centroid model')
 parser.add_argument('-m2', '--centered-instance-path', dest='centered_instance_path', action='store', type=str, default=None, help='path to centered instance model')
+parser.add_argument('-s', '--skel_parts', dest='skel_parts', action='store', type=str, default=None, help='skeleton parts separated by spaces')
 
-parser.add_argument('-p', '--pipeline', dest='pipeline', action='store', type=int, required=True)
 
 # ingesting user-input arguments
 args = parser.parse_args()
@@ -16,11 +16,12 @@ predictions_path = args.predictions_path
 video_path = args.video_path
 centroid_path = args.centroid_path
 centered_instance_path = args.centered_instance_path
+skel_parts = args.skel_parts
 
 sleap_paths = [predictions_path,
                 video_path,
                 centroid_path,
                 centered_instance_path]
 
-exp = dig.Experiment(exp_type='sleap', sleap_paths=sleap_paths)
+exp = dig.Experiment(exp_type='sleap', sleap_paths=sleap_paths, skel_parts=skel_parts)
 exp.sleap_pipeline1()
