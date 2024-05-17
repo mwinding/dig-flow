7e1e074d0cc130e2a33dbf1eb66c30947609ddd4
Michael Winding <michael.winding@crick.ac.uk>
2024-05-17 23:17:03 +0100

    initial commit

---
+++ scripts/sleap_pipeline.py
@@ -0,0 +1,26 @@
+import digflow as dig
+import argparse
+
+# pulling user-input variables from command line
+parser = argparse.ArgumentParser(description='plugcamera pipeline: transferring data from RPis to NEMO, initial processing')
+parser.add_argument('-p', '--predictions-path', dest='predictions_path', action='store', type=str, required=True, help='path to save folder for predictions')
+parser.add_argument('-v', '--video-path', dest='video_path', action='store', type=str, default=None, help='path to folder with video(s)')
+parser.add_argument('-m1', '--centroid-path', dest='centroid_path', action='store', type=str, default=None, help='path to centroid model')
+parser.add_argument('-m2', '--centered-instance-path', dest='centered_instance_path', action='store', type=str, default=None, help='path to centered instance model')
+
+parser.add_argument('-p', '--pipeline', dest='pipeline', action='store', type=int, required=True)
+
+# ingesting user-input arguments
+args = parser.parse_args()
+predictions_path = args.predictions_path
+video_path = args.video_path
+centroid_path = args.centroid_path
+centered_instance_path = args.centered_instance_path
+
+sleap_paths = [predictions_path,
+                video_path,
+                centroid_path,
+                centered_instance_path]
+
+exp = dig.Experiment(exp_type='sleap', sleap_paths=sleap_paths)
+exp.sleap_pipeline1()
---
+++ scripts/sleap_pipeline.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# usage: when transferring from plugcameras 50, 51, and 52 for example, use the following:
+# sbatch --export=EXP_NAME=test_exp,RIG_NUMBERS="50 51 52",IP_FILE=ip_addresses.csv,PIPELINE=2 pipeline.sh
+
+#SBATCH --job-name=slp-pipe
+#SBATCH --ntasks=1
+#SBATCH --time=12:00:00
+#SBATCH --mem=16G
+#SBATCH --partition=ncpu
+#SBATCH --cpus-per-task=8
+#SBATCH --output=slurm-%j.out
+#SBATCH --mail-user=$(whoami)@crick.ac.uk
+#SBATCH --mail-type=FAIL
+
+ml purge
+ml Anaconda3/2023.09-0
+source /camp/apps/eb/software/Anaconda/conda.env.sh
+
+conda activate sleap
+
+# Initialize command with the part that always needs to be executed
+cmd="python sleap_pipeline.py -p "$SAVE_PATH" -v "$VIDEO_PATH" -m1 "$CENTROID" -m2 "$CEN_INS""
+
+# Execute the command and redirect output to log file
+eval $cmd > python_output.log 2>&1
