d928c4d5ffe66c8c0dbd94472210d9d335f806a9
Michael Winding <michael.winding@crick.ac.uk>
2024-07-19 13:55:41 +0100

    added specifics of shelf and amendment system

--- digflow/design.py
+++ digflow/design.py
@@ -4,12 +4,14 @@ import numpy as np
 import os
 import argparse
 import random
+import json
 import tkinter as tk
 from tkinter import messagebox, ttk
 
 class Design:
-    def __init__(self, conditions=None, sample_size=None, file=None, amendment=None):
+    def __init__(self, save_path=None, conditions=None, sample_size=None, experimenters=None, file=None, amendment=None):
 
+        self.save_path = save_path
         self.conditions = conditions
         self.sample_size = sample_size
         self.file = file
@@ -18,20 +20,52 @@ class Design:
         self.all_exps = None
         self.completed_exps = []
         self.remaining_exps = None
-        self.shelves = None
-        self.shelves_df = None
-        #self.experimenters = experimenters
+        self.shelves = []
+        self.shelves_df = pd.DataFrame()
+        self.shelf_template = pd.DataFrame([[ 1,24,  25,48,  49,72],
+                                            [ 2,23,  26,47,  50,71],
+                                            [ 3,22,  27,46,  51,70],
+                                            [ 4,21,  28,45,  52,69],
+                                            [ 5,20,  29,44,  53,68],
+                                            [ 6,19,  30,43,  54,67],
+                                            [ 7,18,  31,42,  55,66],
+                                            [ 8,17,  32,41,  56,65],
+                                            [ 9,16,  33,40,  57,64],
+                                            [10,15,  34,39,  58,63],
+                                            [11,14,  35,38,  59,62],
+                                            [12,13,  36,37,  60,61]])
+        self.shelf_total = 72 # for plugcamera set up
+        self.experimenters = experimenters
 
         # work in progress
         if file!=None:
-            loaded_data = pd.read_csv(file)
-            self.conditions = loaded_data.conditions
-            self.experimenters = loaded_data.experimenters.values[0]
-            self.remaining_exps = loaded_data.remaining_exps
-            self.completed_exps = loaded_data.completed_exps
+            with open(file, 'r') as f:
+                json_data = json.load(f)
+            self.conditions = json_data['conditions']
+            self.experimenters = json_data['experimenters']
+            self.remaining_exps = json_data['remaining']
+            self.completed_exps = json_data['completed']
+
+        # find amendments, e.g. failed experiments and add back to remaining_exps
+        if amendment!=None:
+            self.amendment = pd.read_csv(amendment, index_col=0)
+            amend_bool = self.amendment.amendments == -1
+            to_remove = self.amendment[amend_bool].condition.values
+
+            # Create a copy of the original list to avoid modifying it while iterating
+            filtered_list = self.completed_exps.copy()
+
+            # Iterate over the to_remove list and remove elements from the right side
+            for item in to_remove:
+                # Find the index of the item starting from the end and remove it
+                index = len(filtered_list) - 1 - filtered_list[::-1].index(item)
+                filtered_list.pop(index)
+
+            self.completed_exps = filtered_list
 
         if file==None:
             self.conditions_init(seed=42)
+            self.first_experiment = True
 
     def conditions_init(self, seed):
         random.seed(seed)
@@ -40,20 +74,24 @@ class Design:
         self.remaining_exps = self.all_exps
 
     def add_vials(self, count, day, person):
-        new_row = {'person': person, 
-                            'day': day,
-                            'vials': count}
+        new_row = {'person': person,
+                    'day': day,
+                    'vials': count}
 
         new_row_df = pd.DataFrame([new_row])
         self.vials = pd.concat([self.vials, new_row_df], ignore_index=True)
 
     def build_shelves(self):
         experimenters = np.unique(self.vials.person)
+        random.shuffle(experimenters)
         num_shelves = len(experimenters)
-        for experimenter in experimenters:
-            self.shelves, self.shelves_df = self.build_shelf(experimenter=experimenter)
 
-    def build_shelf(self, experimenter):
+        for i, experimenter in enumerate(experimenters):
+             shelf, shelf_df = self.build_shelf(experimenter=experimenter, shelf_num=i)
+             self.shelves.append(shelf) # need to add meta-data to this...
+             self.shelves_df = pd.concat([self.shelves_df,shelf_df], ignore_index=True)
+
+    def build_shelf(self, experimenter, shelf_num):
 
         pattern = self.vials['person'] == experimenter
         vials_exp = self.vials[pattern]
@@ -95,35 +133,50 @@ class Design:
         random.shuffle(conditions_day2)
 
         # Distribute the conditions across the shelf
-        def fill_columns_with_conditions(shelf, conditions, col_indices):
+        def fill_columns_with_conditions(shelf, conditions, col_indices, shelf_num):
             # Assign the shuffled conditions to the specified columns
+            positions = []
             for idx, (row, col) in enumerate([(row, col) for col in col_indices for row in range(len(shelf.index))]):
                 shelf.iloc[row, col] = conditions[idx]
 
-            return shelf
+                # populate dataframe with non-empty conditions
+                if conditions[idx] != '-':
+                    index = {'experimenter': experimenter, 
+                            'shelf': shelf_num + 1, 
+                            'plugcamera': f'pc{self.shelf_template.iloc[row,col]+(self.shelf_total*shelf_num)}', 
+                            'condition': conditions[idx],
+                            'amendments': '',
+                            'plugcamera_pos': self.shelf_template.iloc[row,col]+(self.shelf_total*shelf_num)}
+
+                    positions.append(index)
+
+            df = pd.DataFrame(positions)    
+            return shelf, df
 
         # Fill columns with conditions
-        shelf_structure = fill_columns_with_conditions(shelf_structure, conditions_day1, [0, 1])
+        shelf_structure, df_1 = fill_columns_with_conditions(shelf_structure, conditions_day1, [0, 1], shelf_num)
         random.shuffle(conditions_day1)
-        shelf_structure = fill_columns_with_conditions(shelf_structure, conditions_day1, [2, 3])
-        shelf_structure = fill_columns_with_conditions(shelf_structure, conditions_day2, [4, 5])
+        shelf_structure, df_2 = fill_columns_with_conditions(shelf_structure, conditions_day1, [2, 3], shelf_num)
+        shelf_structure, df_3 = fill_columns_with_conditions(shelf_structure, conditions_day2, [4, 5], shelf_num)
 
-
-        # Create a dataframe
-        shelf_df = pd.DataFrame(shelf_structure)
-
-        # Flatten the dataframe into a list with positions from 1-72
-        flattened = []
-        for col in shelf_df.columns:
-            for value in shelf_df[col]:
-                flattened.append(value)
-
-        # Create a new dataframe with positions
-        start_position = 1
-        positions = range(start_position, len(flattened) + 1)
-        shelf_df = pd.DataFrame({'plugcamera': [f'pc{x}' for x in positions], 'Value': flattened})
+        shelf_df = pd.concat([df_1, df_2, df_3])
+        shelf_df = shelf_df.sort_values('plugcamera_pos', ascending=True)
+        shelf_df = shelf_df.drop(columns=['plugcamera_pos'])
 
         return shelf_structure, shelf_df
+
+    def output(self):
+        self.shelves_df.to_csv(f'{self.save_path}/shelves.csv')
+        self.shelves[0].to_csv(f'{self.save_path}/shelf1_layout.csv')
+        self.shelves[1].to_csv(f'{self.save_path}/shelf2_layout.csv')
+
+        experiment_dict = {'conditions': self.conditions,
+                            'experimenters': self.experimenters,
+                            'remaining': self.remaining_exps,
+                            'completed': self.completed_exps}
+
+        with open(f'{self.save_path}/experiment.json', 'w') as f:
+            json.dump(experiment_dict, f, indent=4)
 
     def vials_gui(self):
         # function to enter data
@@ -190,7 +243,13 @@ class Design:
         root.mainloop()
 
 # Example usage
-design = Design(sample_size=10, conditions=[f'GAL4-{x}' for x in range(1, 31)])
+design = Design(save_path='./output/',sample_size=10, conditions=[f'GAL4-{x}' for x in range(1, 31)], experimenters=['Lucy','Lena'])
 design.vials_gui()
 design.build_shelves()
+design.output()
+
+# %%
+design2 = Design(save_path='output/',file='output/experiment.json', amendment='output/shelves.csv')
+
+
 # %%
