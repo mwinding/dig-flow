bae1f9a2e04b25678339125457da8272b5c20874
Michael Winding <michael.winding@crick.ac.uk>
2024-09-05 15:15:49 +0100

    fixed nargs bug

--- scripts/plugcamera_experiment-setup.py
+++ scripts/plugcamera_experiment-setup.py
@@ -12,7 +12,7 @@ parser.add_argument('-s', '--save-path', dest='save_path', action='store', type=
 parser.add_argument('-n', '--sample-size', dest='sample_size', action='store', type=str, default=None, help='number of samples required per condition, must be divisible by 3')
 parser.add_argument('-c', '--conditions', dest='conditions', action='store', type=str, default=None, help='conditions to be tested, recommend to be in the tray-position format from the fly stock database')
 parser.add_argument('-e', '--experimenters', dest='experimenters', action='store', type=str, nargs='+', default=['Lucy', 'Lena', 'Alice', 'Anna', 'Michael'], help='names of experimenters')
-parser.add_argument('-cn', '--control_sample_size', dest='control_sample_size', action='store', type=str, nargs='+', help='number of controls per collection')
+parser.add_argument('-cn', '--control_sample_size', dest='control_sample_size', action='store', type=str, help='number of controls per collection')
 
 # ingesting user-input arguments
 args = parser.parse_args()
