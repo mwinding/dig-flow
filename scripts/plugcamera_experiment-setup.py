996f3fc2fab38871c05a23da83a2b29b919411c0
Michael Winding <michael.winding@crick.ac.uk>
2024-09-05 15:14:14 +0100

    added control_num

--- scripts/plugcamera_experiment-setup.py
+++ scripts/plugcamera_experiment-setup.py
@@ -12,6 +12,7 @@ parser.add_argument('-s', '--save-path', dest='save_path', action='store', type=
 parser.add_argument('-n', '--sample-size', dest='sample_size', action='store', type=str, default=None, help='number of samples required per condition, must be divisible by 3')
 parser.add_argument('-c', '--conditions', dest='conditions', action='store', type=str, default=None, help='conditions to be tested, recommend to be in the tray-position format from the fly stock database')
 parser.add_argument('-e', '--experimenters', dest='experimenters', action='store', type=str, nargs='+', default=['Lucy', 'Lena', 'Alice', 'Anna', 'Michael'], help='names of experimenters')
+parser.add_argument('-cn', '--control_sample_size', dest='control_sample_size', action='store', type=str, nargs='+', help='number of controls per collection')
 
 # ingesting user-input arguments
 args = parser.parse_args()
@@ -22,9 +23,10 @@ save_path = args.save_path
 sample_size = int(args.sample_size)
 conditions = args.conditions
 experimenters = args.experimenters
+control_sample_size = int(args.control_sample_size)
 
 if first_run=='True':
-    design = dig.Design(wc_date=wc_date, save_path=save_path,sample_size=sample_size, conditions=conditions, experimenters=experimenters, controls_per_collection=1)
+    design = dig.Design(wc_date=wc_date, save_path=save_path,sample_size=sample_size, conditions=conditions, experimenters=experimenters, controls_per_collection=control_sample_size)
     design.vials_gui()
     design.build_shelves()
     design.output()
